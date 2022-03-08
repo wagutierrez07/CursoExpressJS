7c963fe0c9fed9875b9d12a5349a09f4d75285b6
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-08 14:02:17 -0400

    Routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. (index.ejs) */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* GET nosotros page. (nosotros.ejs) */
+router.get('/nosotros', function(req, res, next) {
+  res.render('nosotros', { title: 'Nosotros somos...' });
+});
+
 
 module.exports = router;
